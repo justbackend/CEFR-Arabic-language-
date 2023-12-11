d2f2dd866a8428c6fcae7729b81964ffb75ed050
Abror Izzatullaev <92588126+justbackend@users.noreply.github.com>
2023-12-11 21:58:00 +0500

    Update settings.py

--- EduExam/settings.py
+++ EduExam/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3!2v-g9se&7q7esjtqu@w565)*w=sirx88kbc17i^ws%bcnf@y'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
